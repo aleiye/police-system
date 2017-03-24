93de4ce93f415aa6051954aff5d9ca5e09da8996
yulu.cui <1281993444@qq.com>
2017-03-24 13:42:37 +0800

    优化

--- public/js/query/alarm_formatter.js
+++ public/js/query/alarm_formatter.js
@@ -15,15 +15,32 @@ function trFormat(data){
     ];
     var types = {
             'A_db_QHQB_T_QHQB_TLDP': function(){
-                values[3] = selfFields['QHQB_T_QHQB_TLDP_FROM_STATION_NAME'];
-                values[5] = formatDate(selfFields['QHQB_T_QHQB_TLDP_TRAIN_DATE'] + selfFields['QHQB_T_QHQB_TLDP_START_TIME']);
-                values[6] = formatDate(data['inserttime']);
-                values[7] = '铁路订票比对';
-                values[8] = data['readStatus'];
+                var newValues = copyArr(values);
+                newValues[3] = selfFields['QHQB_T_QHQB_TLDP_FROM_STATION_NAME'];
+                newValues[5] = formatDate(selfFields['QHQB_T_QHQB_TLDP_TRAIN_DATE'] + selfFields['QHQB_T_QHQB_TLDP_START_TIME']);
+                newValues[6] = formatDate(data['inserttime']);
+                newValues[7] = '铁路订票比对';
+                newValues[8] = data['readStatus'];
+                return newValues;
+            },
+            'A_db_QHQB_T_QHQB_HQJT_DZKPSJB': function(){
+                var newValues = copyArr(values);
+                // newValues[3] = selfFields['QHQB_T_QHQB_TLDP_FROM_STATION_NAME'];
+                // newValues[5] = formatDate(selfFields['QHQB_T_QHQB_TLDP_TRAIN_DATE'] + selfFields['QHQB_T_QHQB_TLDP_START_TIME']);
+                // newValues[6] = formatDate(data['inserttime']);
+                // newValues[7] = '铁路订票比对';
+                // newValues[8] = data['readStatus'];
+                return newValues;
             }
         }
-    types[A_source]();
-    return values;
+    return types[A_source] ? types[A_source]() : values;
+}
+function copyArr(oldArr){
+    var newArr = [];
+    $(oldArr).each(function(i,item){
+        newArr.push(item);
+    });
+    return newArr;
 }
 function add0(m) {
     return m < 10 ? '0' + m : m
